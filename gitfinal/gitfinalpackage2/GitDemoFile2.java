112e19efe1e633af10dcd43c2a5a0a41ce25bb04
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-07 07:16:17 +0000

    done exec

--- gitfinal/gitfinalpackage2/GitDemoFile2.java
+++ gitfinal/gitfinalpackage2/GitDemoFile2.java
@@ -3,6 +3,6 @@ package gitfinalpackage2;
 public class GitDemoFile2 {
 	public static void main(String[] args) {
 		System.out.println("Resource folder script fire");
-	
+		System.out.println("done");
 	}
 }
