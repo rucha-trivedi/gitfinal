c2e55ef70a5629b03b2ef543f7d8a831be7a80f6
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-07 11:54:14 +0000

    bug fixing for shell

--- gitfinal/gitfinalpackage2/GitDemoFile2.java
+++ gitfinal/gitfinalpackage2/GitDemoFile2.java
@@ -2,6 +2,6 @@ package gitfinalpackage2;
 
 public class GitDemoFile2 {
 	public static void main(String[] args) {
-		System.out.println("test existing directory from script");
+		System.out.println("Inside Directory");
 	}
 }
