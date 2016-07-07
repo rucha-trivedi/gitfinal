1dd736344e71dde92314ff5df8fe0e93106151af
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-07 07:00:11 +0000

    script path changes

--- gitfinal/gitfinalpackage2/GitDemoFile2.java
+++ gitfinal/gitfinalpackage2/GitDemoFile2.java
@@ -4,5 +4,6 @@ public class GitDemoFile2 {
 	public static void main(String[] args) {
 		System.out.println("Resource file script fire");
 		System.out.println("Script failed! ");
+		System.out.println("Script path changed");
 	}
 }
