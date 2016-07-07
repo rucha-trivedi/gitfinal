b975cca19464b7c766431e190367d5e9f1fc23c2
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-07 06:34:21 +0000

    script failed solution

--- gitfinal/gitfinalpackage2/GitDemoFile2.java
+++ gitfinal/gitfinalpackage2/GitDemoFile2.java
@@ -3,5 +3,6 @@ package gitfinalpackage2;
 public class GitDemoFile2 {
 	public static void main(String[] args) {
 		System.out.println("Resource file script fire");
+		System.out.println("Script failed! ");
 	}
 }
