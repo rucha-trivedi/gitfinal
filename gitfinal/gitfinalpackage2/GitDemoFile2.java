a91fe0380a523abc6e9af4e15c44e6866ff52aa1
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-07 07:06:30 +0000

    removed lines and changed shell

--- gitfinal/gitfinalpackage2/GitDemoFile2.java
+++ gitfinal/gitfinalpackage2/GitDemoFile2.java
@@ -2,8 +2,7 @@ package gitfinalpackage2;
 
 public class GitDemoFile2 {
 	public static void main(String[] args) {
-		System.out.println("Resource file script fire");
-		System.out.println("Script failed! ");
-		System.out.println("Script path changed");
+		System.out.println("Resource folder script fire");
+	
 	}
 }
