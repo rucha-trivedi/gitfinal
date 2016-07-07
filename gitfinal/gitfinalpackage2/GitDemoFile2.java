95a69bece52bb5862ea3bdcabaad94d951ae7757
rucha-trivedi <rucha.trivedi@workhiveapp.com>
2016-07-07 10:23:54 +0000

    console test

--- gitfinal/gitfinalpackage2/GitDemoFile2.java
+++ gitfinal/gitfinalpackage2/GitDemoFile2.java
@@ -2,9 +2,6 @@ package gitfinalpackage2;
 
 public class GitDemoFile2 {
 	public static void main(String[] args) {
-		System.out.println("Resource folder script fire");
-		System.out.println("Done");
-		System.out.println(" Mismatched Properties ");
-		System.out.println("Already Added");
+		System.out.println("Console test");
 	}
 }
